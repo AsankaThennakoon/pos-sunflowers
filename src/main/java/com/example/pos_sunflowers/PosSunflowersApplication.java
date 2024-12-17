433a471067b28d9cd297d5c5199e650da409a2fc
AsankaThennakoon <newsunflowers98@gmail.com>
2024-12-17 13:57:46 +0530

    init the project

---
+++ src/main/java/com/example/pos_sunflowers/PosSunflowersApplication.java
@@ -0,0 +1,13 @@
+package com.example.pos_sunflowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSunflowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosSunflowersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pos_sunflowers/PosSunflowersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pos_sunflowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSunflowersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
